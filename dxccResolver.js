9aae4c0131d443b77e9c75410a6f9a3b809c50f9
Henrique Gravina <henrique@gravina.com.br>
2023-02-15 18:19:08 +0000

    Add more Countries/Entities

--- dxccResolver.js
+++ dxccResolver.js
@@ -1,6 +1,6 @@
 
 // Author: Henrique B. Gravina
-// This code is base on 2013_Current_Deleted.txt from ARRL
+// This code is base on 2013_Current_Deleted.txt from ARRL and other sites in the WWW
 // 2023/02/15
 
 class DxccEntity {
@@ -40,9 +40,13 @@ class DxccEntities {
       //Denmark
       new DxccEntity(/^(O[U-W]|OZ)/,"Denmark", 221),
 
-        // England
+      //  ON-OT*              Belgium                            EU    27    14    209
+      //Denmark
+      new DxccEntity(/^(O[N-T])/,"Belgium", 209),
+
+      // England
       new DxccEntity(/^M[A-Z,0-9]/,"England", 223   ),
-      new DxccEntity(/^(?!(GD|GT|GI|GN|GJ|GH|GM|GS|GU|GP|GW|GC))(G\d|GX\d|G[M-Z]\d)/,"England", 223   ),
+      new DxccEntity(/^(?!(GD|GT|GI|GN|GJ|GH|GM|GS|GU|GP|GW|GC))(G\d|GX\d|G[BM-Z]\d)/,"England", 223   ),
       new DxccEntity(/^(GD|GT)[A-Z\d]*/,"Isle of Man", 114   ),
       new DxccEntity(/^(GI|GN)[A-Z\d]*/,"Northern Ireland", 265   ),
       new DxccEntity(/^(GJ|GH)[A-Z\d]*/,"Jersey", 122   ),
@@ -50,8 +54,16 @@ class DxccEntities {
       new DxccEntity(/^(GU|GP)[A-Z\d]*/,"Guernsey", 106   ),
       new DxccEntity(/^(GW|GC)[A-Z\d]*/,"Wales", 294   ),
 
-        //Slovenia S5(6)*              Slovenia
-      new DxccEntity(/^S5[67]/,"Slovenia", 499   ),
+      // Russia
+
+      //UA-UI1-7,RA-RZ*     European Russia                    EU    (E)   16    054
+      //new DxccEntity(/^/,"European Russia", 54   ),
+      //UA2,RA2*            Kaliningrad                        EU    29    15    126
+      new DxccEntity(/^U[A-I][890]/,"Asiatic Russia", 15  ),
+      new DxccEntity(/^R[890]/,"Asiatic Russia", 15  ),
+
+
+      new DxccEntity(/^S5[0-9]/,"Slovenia", 499   ),
       new DxccEntity(/^OE[0-9]/,"Austria", 206   ),
       new DxccEntity(/^F[0-9]/,"France", 227   ),
       new DxccEntity(/^FR[0-9]/,"Reunion I.", 453   ),
@@ -67,14 +79,14 @@ class DxccEntities {
 
       
       //Brazil
-      new DxccEntity(/^P[P-Y][1235689]/,"Brasil", 108   ),
-      new DxccEntity(/^Z[V-Z][1235689]/,"Brasil", 108   ),
+      new DxccEntity(/^P[P-Y][0-9]/,"Brasil", 108   ),
+      new DxccEntity(/^Z[V-Z][0-9]/,"Brasil", 108   ),
       new DxccEntity(/^PY0F/,"Fernando de Noronha", 56   ),
       new DxccEntity(/^PY0S/,"St. Peter & St. Paul Rocks", 253   ),
       new DxccEntity(/^PY0T/,"Trindade & Martim Vaz Is.", 273   ),
 
 
-      new DxccEntity(/^U[A-I][890]/,"Asiatic Russia", 15   ),
+      
       new DxccEntity(/^VU[12356890]/,"India", 324   ),
       new DxccEntity(/^4[XZ][12356890]/,"Israel", 336   ), 
       new DxccEntity(/^O[F-I]/,"Finland", 224   ),      
@@ -91,16 +103,49 @@ class DxccEntities {
       new DxccEntity(/^P[A-I]/,"Netherlands", 263   ),
       new DxccEntity(/^E7[2-9]/,"Bosnia-Herzegovina", 501   ),
 
+      //Slovak Republic
+      new DxccEntity(/^OM[0-9]/,"Slovak Republic", 504   ),
+      //Czech Republic
+      new DxccEntity(/^(O[K-L][0-9])/,"Czech Republic", 503   ),
+      //Norway
+      new DxccEntity(/^(L[A-N][0-9])/,"Norway", 266   ),
       // Sweden
       new DxccEntity(/^((S[A-M][0-9])|7S|8S)/,"Sweden", 284   ),
       // Poland
-      new DxccEntity(/^S[N-R][0-9]/,"Poland", 269   ),
+      new DxccEntity(/^(S[N-R]|HF|3Z)[0-9]/,"Poland", 269   ),
       // Syria
       new DxccEntity(/^YK[0-9]/,"Syria", 384   ),
       // Latvia 
       new DxccEntity(/^YL[0-9]/,"Latvia", 145   ),
+      //Hungary  
+      new DxccEntity(/^(HA|HG)[0-9]/,"Hungary",  239  ),
+      //Switzerland  
+      new DxccEntity(/^HB[0-9]/,"Switzerland", 287   ),
+      //Turkey
+      new DxccEntity(/^T[A-C][0-9]/,"Turkey", 390   ),
+      //Iceland
+      new DxccEntity(/^TF[0-9]/,"Iceland", 242   ),
+      //Corsica
+      new DxccEntity(/^TK[0-9]/,"Corsica", 214   ),
+      //Kazakhstan
+      new DxccEntity(/^U[N-Q][0-9]/,"Kazakhstan", 130   ),
+      //Ukraine
+      new DxccEntity(/^((U[R-Z])|(E[M-O]))[0-9]/,"Ukraine", 288   ),
+      //Zimbabwe
+      new DxccEntity(/^Z2[0-9]/,"Zimbabwe", 452),
+      //Macedonia
+      new DxccEntity(/^Z3[0-9]/,"Macedonia", 502),
+      //South Africa
+      new DxccEntity(/^(Z[R-U])[0-9]/,"South Africa", 462   ),
 
-      
+      //Luxembourg
+      new DxccEntity(/^LX[0-9]/,"Luxembourg", 254),
+      //Lithuania
+      new DxccEntity(/^LY[0-9]/,"Lithuania", 146),
+      //Bulgaria
+      new DxccEntity(/^LZ[0-9]/,"Bulgaria", 212),
+      //Benin
+      new DxccEntity(/^TY[0-9]/,"Benin", 416),
 
     ];
   }
@@ -114,13 +159,22 @@ class DxccEntities {
     return { callsign, entity: "Unknown", dxcc: null };
   }
   
+  getDxccFromCallsign(callsign) {
+    for (const dxccEntity of this.dxccEntities) {
+      if (callsign.match(dxccEntity.prefix)) {
+        return dxccEntity.dxcc;
+      }
+    }
+    return null ;
+  }
+
   getCountryFromDxcc(dxcc_nmber) {
     for (const dxccEntity of this.dxccEntities) {
       if (dxcc_nmber == dxccEntity.dxcc) {
-        return { entity: dxccEntity.entity, dxcc: dxccEntity.dxcc };
+        return dxccEntity.entity;
       }
     }
-    return { entity: "Unknown", dxcc: null };
+    return "Unknown";
   }
 }
 
