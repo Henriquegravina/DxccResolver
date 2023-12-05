9176d40f2e8e49012ec7bceaed82ed9029094571
Henrique Gravina <henrique@gravina.com.br>
2023-12-05 19:02:49 +0000

    add some more tests

--- test/call.test.js
+++ test/call.test.js
@@ -7,5 +7,16 @@ test("Return Json from a CALL", async () => {
   expect(dxcc.getEntityFromCallsign("PU3IKE")).toEqual({ entity: "Brasil" });
   expect(dxcc.getEntityFromCallsign("PY3WW")).toEqual({ entity: "Brasil" });
   expect(dxcc.getEntityFromCallsign("PR8WW")).toEqual({ entity: "Brasil" });
-  
+
+  //Israel
+  expect(dxcc.getEntityFromCallsign("4Z75TL")).toEqual({ entity: "Israel" });
+
+  //Portugal
+  expect(dxcc.getEntityFromCallsign("CT2GVH")).toEqual({ entity: "Portugal" });
+
+  //Spain
+  expect(dxcc.getEntityFromCallsign("EA3EA")).toEqual({ entity: "Spain" });
+
+  //France
+  expect(dxcc.getEntityFromCallsign("F4HAB")).toEqual({ entity: "France" });
 });
